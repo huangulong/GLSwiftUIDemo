d173528c421e460630384c5e3cfa1bef40942492
huanggulong 2 <huanggulong@xueleyun.com>
2019-10-21 10:14:33 +0800

    Initial Commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by admin on 2019/10/21.
+//  Copyright © 2019 历山大亚. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
